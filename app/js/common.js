fba1c1d43223fb15bdbc9698d97307226334b132
Dmitry Toshchev <dterron@yandex.ru>
2018-06-13 13:31:19 +0300

    finished all header hidden section

--- app/js/common.js
+++ app/js/common.js
@@ -16,13 +16,31 @@ $(function() {
 		$('.show-attorneys').toggleClass('mouseOver');
 	});
 
-	// Show attorneys on hover
+	// Show services on hover
 	$('.show-services').hover(function(){
 		$('.serv-hid').toggleClass('visible');
 		$(this).toggleClass('mouseOver');
 	});
 	$('.serv-hid').hover(function(){
 		$('.show-services').toggleClass('mouseOver');
+	});
+
+	// Show insights on hover
+	$('.show-insights').hover(function(){
+		$('.ins-hid').toggleClass('visible');
+		$(this).toggleClass('mouseOver');
+	});
+	$('.ins-hid').hover(function(){
+		$('.show-insights').toggleClass('mouseOver');
+	});
+
+	// Show insights on hover
+	$('.show-about').hover(function(){
+		$('.about-hid').toggleClass('visible');
+		$(this).toggleClass('mouseOver');
+	});
+	$('.about-hid').hover(function(){
+		$('.show-about').toggleClass('mouseOver');
 	});
 
 
